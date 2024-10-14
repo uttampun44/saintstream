46f2e99920f9578a528fc93de7a288ef621d1913
uttampun <uttampun50@gmail.com>
2024-10-14 17:10:43 +0545

    home page wallpaper

--- saint-frontend/src/page/Home.tsx
+++ saint-frontend/src/page/Home.tsx
@@ -20,17 +20,17 @@ export default function Home() {
 
   const responsive = {
    superLargeDesktop: {
-      // the naming can be any, depends on you.
+     
       breakpoint: { max: 4000, min: 3000 },
-      items: 5
+      items: 1
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3
+      items: 1
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2
+      items: 1
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
@@ -43,30 +43,30 @@ export default function Home() {
      <Layout >
         
    
-       <Carousel responsive={responsive}>
+       
        {
-          movies.splice(0, 5).map((movie:any, index) => (
-             <section className={cn("absolute -z-10 top-0 w-full")}  style={{
-               backgroundImage: `url(http://image.tmdb.org/t/p/original/${movie.poster_path})`,
-               backgroundRepeat: "no-repeat",
-               backgroundSize: "100%",
-               backgroundPosition: "center",
-               height: "100vh",
-              }}>
-           <div className={cn("fixed w-full h-full inset-0 bg-black/90 -z-10")}></div>
-            <div className={cn("homeContainer max-w-[1440px] mx-auto relative p-5")}>
+         movies.splice(0,1).map((movie:any, index:any) => (
+           <section key={index} className={cn("absolute -z-10 top-0 w-full")}  style={{
+             backgroundImage: `url(http://image.tmdb.org/t/p/original/${movie.poster_path})`,
+             backgroundRepeat: "no-repeat",
+             backgroundSize: "100%",
+             backgroundPosition: "center",
+             height: "100vh",
+            }}>
+                <div className={cn("fixed w-full h-full inset-0 bg-black/90 -z-10")}></div>
+            <div className={cn("homeContainer max-w-[1440px] mx-auto p-5")}>
                <div className="grid h-[700px] items-end justify-start max-w-2xl w-full">
                  <div className="title text-slate-500">
-                     {/* <h2 className={cn("text-white text-4xl font-bold my-1")}>{data.original_title}</h2>
-                     {data.runtime} minutes
-                     <p className={cn("text-gray-400 font-normal text-lg")}>{data.overview}</p> */}
+                     <h2 className={cn("text-white text-4xl font-bold my-1")}>{movie.original_title}</h2>
+                     {movie.runtime} minutes
+                     <p className={cn("text-gray-400 font-normal text-lg")}>{movie.overview}</p>
                  </div>
                </div>
             </div>
-         </section>
+          </section>
        ))
       }
-      </Carousel>
+    
      </Layout>
   
   );
