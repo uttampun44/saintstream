b8d8dce6549bfa8acbc51cfe7581b6b1c7e9a858
uttam pun <uttampun50@gmail.com>
2024-11-15 21:37:09 +0545

    remove path

--- saint-frontend/vite.config.ts
+++ saint-frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       "/api":{
         target: "https://saintstream-production.up.railway.app",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+
       }
       
     }
