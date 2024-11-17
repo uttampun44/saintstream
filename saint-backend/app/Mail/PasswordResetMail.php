c7750340e05bfdd18b620e1f1c4d71bc7ddfbb2b
uttam pun <uttampun50@gmail.com>
2024-11-17 19:55:54 +0545

    resetpassword done

--- saint-backend/app/Mail/PasswordResetMail.php
+++ saint-backend/app/Mail/PasswordResetMail.php
@@ -41,7 +41,7 @@ class PasswordResetMail extends Mailable
      */
     public function content(): Content
     {
-        $resetLink = 'http://localhost:3000/reset-password?token=' . $this->token;
+        $resetLink = 'https://saintstream-eight.vercel.ap/reset-password?token=' . $this->token;
 
         return new Content(
             view: 'emails', 
